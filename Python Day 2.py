790582a69817cba4220f388f72a691a9b1e9683d
sfe25am066-wq <sfe25am066@hcctrichy.ac.in>
2026-08-30 18:31:54 +0530

    Add Python Day 2

---
+++ Python Day 2.py
@@ -0,0 +1,17 @@
+# Variables and Data Types
+name="Noothana"
+age=18
+height=143.5
+student=True
+print("Name:",name)
+print("Age:",age)   
+print("Height:",height) 
+print("Student:",student)   
+print("Data type of name:",type(name))  
+print("Data type of age:",type(age))
+print("Data type of height:",type(height))      
+print("Data type of student:",type(student))    
+name=input("Enter your name: ") 
+age=int(input("Enter your age: "))  
+print("Hello",name)
+print("Your age is ",age)    
