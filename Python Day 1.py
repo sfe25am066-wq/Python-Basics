a4f032bcbcdeba3b73807afc6faa1da50404fc7d
sfe25am066-wq <sfe25am066@hcctrichy.ac.in>
2026-08-30 17:59:12 +0530

    Add Python Day 1

---
+++ Python Day 1.py
@@ -0,0 +1,25 @@
+print("Hello, World!")
+name="Noothana"
+age=18
+course="Python"
+print(name)
+print(age)
+print(course)
+a=10
+b=5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)  
+print(a**b) 
+print(a//b) 
+print(a>b)  
+print(a<b)  
+print(a==b) 
+print(a!=b) 
+print(a>=b) 
+print(a<=b) 
+print(a and b)
+print(a or b)
+print(not a)
